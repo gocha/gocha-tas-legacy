7429278f6cd1e92bdd89b3b2731f2142ff6eba0e
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2009-09-23 15:48:12 +0000

    add histrical tools for the time being. they should be removed in near future.
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@43 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

---
+++ Tools/smvcat/src/smvcat.h
@@ -0,0 +1,16 @@
+/**
+ * smvcat.h: smv concatenator
+ */
+
+#ifndef SMVCAT_H
+#define SMVCAT_H
+
+
+#include "cioutil.h"
+
+
+void cappShowUsage(void);
+int main(int argc, char* argv[]);
+
+
+#endif /* !SMVCAT_H */
---
+++ Tools/smvoptim/src/smvoptim.h
@@ -0,0 +1,15 @@
+/**
+ * smvoptim.h: truncates redundant records in smv
+ */
+
+#ifndef SMVOPTIM_H
+#define SMVOPTIM_H
+
+
+#include "cioutil.h"
+
+
+bool smvoptim(const char* path);
+
+
+#endif /* !SMVOPTIM_H */
