68aefd47391c598c537fd12f67496b15d5149db3
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2009-10-12 13:02:56 +0000

    add some old game-specific scripts
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@63 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

---
+++ Scripts/gra3mouse.lua
@@ -0,0 +1,126 @@
+--[[
+
+ Gradius 3 + Mouse Control for Snes9x 1.43-rr v16
+ This script looks working good on both (U) and (J) ROM.
+
+ SHOT/MISSILE   LEFT CLICK
+ POWERUP        RIGHT CLICK
+
+ CAUTION!
+ Do NOT change pad settings via OPTION. I don't care them at all.
+
+ gocha -- http://tasvideos.org/forum/r/gocha
+
+]]--
+
+-- SCRIPT OPTION
+local shotBehavior = 0 -- 0=auto, 1=leftClick(hold), 2=leftClick(trigger), 3=disable
+local allowMouseAlways = true
+local renderCursorInGame = false -- for video logging
+
+gui.opacity(0.6)
+
+--
+
+if not emu then emu = snes9x end
+if not emu then
+	error("This script runs under snes9x")
+end
+
+-- draw traditional arrow cursor (for video logging) ;)
+function gui.drawarrowcursor(x, y, color, outlineColor)
+	if color == nil then color = "white" end
+	if outlineColor == nil then outlineColor = "black" end
+	gui.line(x, y, x, y+16, outlineColor)
+	gui.line(x+1, y+1, x+11, y+11, outlineColor)
+	gui.line(x+1, y+15, x+3, y+13, outlineColor)
+	gui.line(x+7, y+11, x+10, y+11, outlineColor)
+	gui.line(x+4, y+12, x+7, y+19, outlineColor)
+	gui.line(x+7, y+12, x+10, y+19, outlineColor)
+	gui.line(x+8, y+20, x+9, y+20, outlineColor)
+	gui.line(x+1, y+2, x+1, y+14, color)
+	gui.line(x+2, y+3, x+2, y+13, color)
+	gui.line(x+3, y+4, x+3, y+12, color)
+	gui.line(x+4, y+5, x+4, y+11, color)
+	gui.line(x+5, y+6, x+5, y+13, color)
+	gui.line(x+6, y+7, x+6, y+15, color)
+	gui.line(x+7, y+8, x+7, y+10, color)
+	gui.line(x+8, y+9, x+8, y+10, color)
+	gui.pixel(x+9, y+10, color)
+	gui.line(x+7, y+14, x+7, y+17, color)
+	gui.line(x+8, y+16, x+8, y+19, color)
+	gui.line(x+9, y+18, x+9, y+19, color)
+end
+function gui.drawarrowcursorwithshadow(x, y)
+	gui.drawarrowcursor(x+1, y+1, "#00000080", "#00000080")
+	gui.drawarrowcursor(x, y)
+end
+
+local key, keyprev = {}, {}
+local mouse, mouseprev = {}, {}
+local gra3State = memory.readbyte(0x7e0070)
+local gra3RewriteBehavior = false
+local gra3Paused = false
+emu.registerbefore(function()
+	key = input.get()
+	mouse = { x = key.xmouse, y = key.ymouse }
+	key.xmouse, key.ymouse = nil, nil
+
+	gra3RewriteBehavior = false
+
+	local padId = (memory.readbyte(0x7e007a) / 2) + 1
+	if padId < 1 or padId > 2 then return end
+	local pad = joypad.get(padId)
+
+	gra3Paused = (memory.readbyte(0x7e0066) ~= 0)
+	gra3State = memory.readbyte(0x7e0070)
+	if gra3State ~= 6 then
+		if allowMouseAlways then
+			if key.leftclick then pad.A, pad.start = 1, 1 end
+			if key.rightclick then pad.select = 1 end
+			joypad.set(padId, pad)
+		end
+		return
+	end
+
+	gra3RewriteBehavior = true
+
+	local x, y = mouse.x, mouse.y
+	if not gra3Paused then
+		pad.B = nil
+		if shotBehavior == 3 then
+			-- disable shooting
+		elseif shotBehavior == 1 then
+			if key.leftclick then pad.B = 1 end
+		elseif shotBehavior == 2 then
+			if key.leftclick and not keyprev.leftclick then pad.B = 1 end
+		else
+			pad.B = 1
+			if key.leftclick then pad.B = nil end
+		end
+		if key.rightclick and not keyprev.rightclick then
+			pad.A = 1
+		else
+			pad.A = nil
+		end
+		pad.left = 1 -- hold something to move options
+		joypad.set(padId, pad)
+	end
+
+	if y >= 224 then y = 255 - 16 end
+	memory.writebyte(0x7e020a, x)
+	memory.writebyte(0x7e020e, y + 16)
+end)
+
+emu.registerafter(function()
+	keyprev = key
+	mouseprev = mouse
+end)
+
+gui.register(function()
+	if renderCursorInGame then
+		gui.drawarrowcursorwithshadow(mouse.x, mouse.y)
+	end
+end)
+
+while true do emu.frameadvance() end
---
+++ Scripts/smwcheat.lua
@@ -0,0 +1,543 @@
+--[[
+
+ Super Mario World (U) Cheat Script by gocha
+ Operation check done by using snes9x-rr 1.43 v17 svn
+   http://code.google.com/p/snes9x143-rerecording/
+ Last modified: 2009-10-12 (WIP2.1.1, just modified for new lua engine; minor bugfixes)
+
+ === Cheat Keys ===
+
+ Level:
+ up+select         Powerup (small,big,cape,fire)
+ L+A               Change move method (normal.P-meter,free)
+ pause+select      Exit the current level, with activating the next level
+ pause+(A+select)  Exit the current level, with activating the next level (secret goal)
+ pause+(B+select)  Exit the current level, without activating the next level
+
+ Note: color blocks can't be activated if you beat a switch palace with the pause-exit cheat.
+
+ === Other Features ===
+
+ - P-meter display
+ - Cut powerup / powerdown animation
+ - Display some other useful informations (holding L+R in 0.5s may switch info view)
+
+ Some of these features can be enabled / disabled by modifying the following option settings.
+
+]]--
+
+-- option start here >>
+
+local smwCutPowerupAnimationOn = true
+local smwCutPowerdownAnimationOn = true
+
+local smwShowInfoMore = false -- if true, displays some of useful informations (hopefully)
+local smwShowInfoMoreKeySw = false -- if true, holding L+R in 0.5s switches info view
+
+-- Move speed definitions for free move mode.
+-- I guess you usually won't need to modify these.
+local smwFreeMoveSpeed = 2.0 -- px/f
+local smwFreeMoveSpeedupMax = 4.0 -- px/f
+local smwFreeMovePMeterLength = 1 -- frame(s)
+
+-- << options end here
+
+-- [ generic utility functions ] -----------------------------------------------
+
+if not emu then emu = snes9x end
+if not emu then
+    error("This script runs under snes9x")
+end
+
+if not bit then
+    require("bit")
+end
+
+-- copy a table
+function table_clone_recursive_(e)
+    if type(e) == "table" then
+        local newTable = {}
+        for k, v in pairs(e) do
+            newTable[k] = table_clone_recursive_(v)
+        end
+        return newTable
+    else
+        return e
+    end
+end
+table.clone = function (e)
+    if type(e) == "table" then
+        return table_clone_recursive_(e)
+    else
+        error("bad argument #1 to 'table.clone' (table expected, got " .. type(e) .. ")")
+    end
+end
+
+-- convert table to string (ex. {name=gocha, age=4})
+function table_dump_recursive_(e)
+    if type(e) == "table" then
+        local str = ""
+        local first = true
+        local list = true
+        local lastKey = 0
+        for k in pairs(e) do
+            if type(k) ~= "number" or k - lastKey ~= 1 then
+                list = false
+                break
+            end
+            lastKey = k
+        end
+        for k, v in pairs(e) do
+            if not first then
+                str = str .. ", "
+            else
+                first = false
+            end
+            if not list then
+                str = str .. table_dump_recursive_(k) .. "="
+            end
+            if type(v) == "string" then
+               str = str .. '"' .. table_dump_recursive_(v) .. '"'
+            else
+               str = str .. table_dump_recursive_(v)
+            end
+        end
+        --if list then
+        --    return "(" .. str .. ")"
+        --else
+            return "{" .. str .. "}"
+        --end
+    else
+        return tostring(e)
+    end
+end
+table.dump = function (e)
+    if type(e) == "table" then
+        return table_dump_recursive_(e)
+    else
+        error("bad argument #1 to 'table.dump' (table expected, got " .. type(e) .. ")")
+    end
+end
+
+-- [ gameemu lua utility functions ] -------------------------------------------
+
+function gui.edgelessbox(x1, y1, x2, y2, colour)
+    gui.line(x1+1, y1, x2-1, y1, colour) -- top
+    gui.line(x2, y1+1, x2, y2-1, colour) -- right
+    gui.line(x1+1, y2, x2-1, y2, colour) -- bottom
+    gui.line(x1, y1+1, x1, y2-1, colour) -- left
+end
+
+local pad_all_presses = { "start", "select", "up", "down", "left", "right", "A", "B", "X", "Y", "L", "R" }
+
+local pad_max = 2
+local pad_press, pad_down, pad_up, pad_prev, pad_send = {}, {}, {}, {}, {}
+local pad_presstime = {}
+for player = 1, pad_max do
+    pad_press[player] = {}
+    pad_presstime[player] = { start=0, select=0, up=0, down=0, left=0, right=0, A=0, B=0, X=0, Y=0, L=0, R=0 }
+end
+
+-- scan button presses
+function scanJoypad()
+    for i = 1, pad_max do
+        pad_prev[i] = table.clone(pad_press[i])
+        pad_press[i] = joypad.read(i)
+        pad_send[i] = table.clone(pad_press[i])
+        -- scan keydowns
+        pad_down[i] = {}
+        for k in pairs(pad_press[i]) do
+            if not pad_prev[i][k] then
+                pad_down[i][k] = 1
+            end
+        end
+        -- scan keyups
+        pad_up[i] = {}
+        for k in pairs(pad_prev[i]) do
+            if not pad_press[i][k] then
+                pad_up[i][k] = 1
+            end
+        end
+        -- count press length
+        for k, v in ipairs(pad_all_presses) do
+            if not pad_press[i][v] then
+                pad_presstime[i][v] = 0
+            else
+                pad_presstime[i][v] = pad_presstime[i][v] + 1
+            end
+        end
+    end
+end
+-- send button presses
+function sendJoypad()
+    for i = 1, pad_max do
+        joypad.set(i, pad_send[i])
+    end
+end
+
+-- append button presses
+function AppendJoypadPresses(player, ...)
+    local args = {...}
+    for k, v in ipairs(args) do
+        pad_send[player][v] = 1
+    end
+end
+
+-- delete button presses
+function DeleteJoypadPresses(player, ...)
+    local args = {...}
+    for k, v in ipairs(args) do
+        pad_send[player][v] = nil
+    end
+end
+
+-- toggle button presses
+function ToggleJoypadPresses(player, ...)
+    local args = {...}
+    for k, v in ipairs(args) do
+        if not pad_send[player][v] then
+            pad_send[player][v] = 1
+        else
+            pad_send[player][v] = nil
+        end
+    end
+end
+
+-- [ game-specific utility functions ] -----------------------------------------
+
+local moveMethod_normal = 0
+local moveMethod_pmeter = 1
+local moveMethod_free = 2
+local moveMethod_max = 3
+
+local smwMoveMethod = moveMethod_normal
+local smwFreeMovePMeter = 0
+
+local RAM_frameCountAlt = 0x7e0014
+local RAM_gameMode = 0x7e0100
+local RAM_player = 0x7e0db3
+local RAM_powerup = 0x7e0019
+local RAM_pMeter = 0x7e13e4
+local RAM_takeOffMeter = 0x7e149f
+local RAM_starInvCount = 0x7e1490
+local RAM_hurtInvCount = 0x7e1497
+local RAM_xSpeed = 0x7e007b
+local RAM_ySpeed = 0x7e007d
+local RAM_xPos = 0x7e0094
+local RAM_yPos = 0x7e0096
+local RAM_xSubPos = 0x7e13da
+local RAM_ySubPos = 0x7e13dc
+local RAM_movement = 0x7e0071
+local RAM_lockSpritesTimer = 0x7e009d
+local RAM_marioFrameCount = 0x7e1496
+local RAM_frozen = 0x7e13fb
+local RAM_paused = 0x7e13d4
+local RAM_levelIndex = 0x7e13bf
+local RAM_levelFlagTable = 0x7e1ea2
+local RAM_typeOfExit = 0x7e0dd5
+local RAM_midwayPoint = 0x7e13ce
+local RAM_activateNextLevel = 0x7e13ce
+
+local pMeter_max = 112
+local takeOffMeter_max = 112
+
+local gameMode_ow  = 14
+local gameMode_level = 20
+
+local smwPlayerPrev, smwPlayer, smwPlayerChanged
+local smwGameModePrev, smwGameMode, smwGameModeChanged
+local smwPausePrev, smwPause, smwPauseChanged
+local smwMovementPrev, smwMovement, smwMovementChanged
+-- scan some parameters that control behavior of the script
+function smwScanStatus()
+    smwPlayerPrev = smwPlayer
+    smwPlayer = memory.readbyte(RAM_player) + 1
+    smwPlayerChanged = (smwPlayer ~= smwPlayerPrev)
+    smwGameModePrev = smwGameMode
+    smwGameMode = memory.readbyte(RAM_gameMode)
+    smwGameModeChanged = (smwGameMode ~= smwGameModePrev)
+    smwPausePrev = smwPause
+    smwPause = (memory.readbyte(RAM_paused) ~= 0)
+    smwPauseChanged = (smwPause ~= smwPausePrev)
+    smwMovementPrev = smwMovement
+    smwMovement = memory.readbyte(RAM_movement)
+    smwMovementChanged = (smwMovement ~= smwMovementPrev)
+end
+
+-- increment powerup
+function smwDoPowerUp()
+    local powerup = memory.readbyte(RAM_powerup)
+    memory.writebyte(RAM_powerup, (powerup + 1) % 4)
+end
+
+-- set move method
+function smwSetMoveMethod(mode)
+    -- cleanups
+    if smwMoveMethod == moveMethod_free then
+        memory.writebyte(RAM_frozen, 0) -- unfreeze Mario
+    end
+    -- apply new method
+    smwMoveMethod = mode % moveMethod_max
+end
+
+-- normal move
+function smwMoveNormalProc()
+    -- do nothing
+end
+
+-- P-meter mode
+function smwMovePMeterProc()
+    memory.writebyte(RAM_pMeter, pMeter_max)
+    memory.writebyte(RAM_takeOffMeter, takeOffMeter_max+1)
+end
+
+-- count own pmeter for 
+function smwFreeMovePMeterCount()
+    local move = pad_press[smwPlayer].left or pad_press[smwPlayer].right
+        or pad_press[smwPlayer].up or pad_press[smwPlayer].down
+
+    -- count up own P-meter
+    if pad_press[smwPlayer].Y and move then
+        if smwFreeMovePMeter < smwFreeMovePMeterLength then
+            smwFreeMovePMeter = smwFreeMovePMeter + 1
+        end
+    else
+        smwFreeMovePMeter = 0
+    end
+end
+
+-- Free move mode
+function smwMoveFreeProc()
+    local x = memory.readword(RAM_xPos) + (memory.readbyte(RAM_xSubPos)/256.0)
+    local y = memory.readword(RAM_yPos) + (memory.readbyte(RAM_ySubPos)/256.0)
+    local speed, xv, yv = 0.0, 0.0, 0.0
+
+    -- calc Mario's new position
+    smwFreeMovePMeterCount()
+    speed = smwFreeMoveSpeed + (smwFreeMoveSpeedupMax * smwFreeMovePMeter / smwFreeMovePMeterLength)
+    if pad_press[smwPlayer].left  then xv = xv - speed end
+    if pad_press[smwPlayer].right then xv = xv + speed end
+    if pad_press[smwPlayer].up    then yv = yv - speed end
+    if pad_press[smwPlayer].down  then yv = yv + speed end
+
+    -- freeze Mario
+    if smwMovement == 0 then
+        memory.writebyte(RAM_frozen, 1)
+        memory.writebyte(RAM_xSpeed, 0)
+        memory.writebyte(RAM_ySpeed, 0)
+        x, y = x + xv, y + yv
+
+        -- but animate sprites
+        memory.writebyte(RAM_frameCountAlt, (memory.readbyte(RAM_frameCountAlt) + 1) % 256)
+    else
+        memory.writebyte(RAM_frozen, 0)
+    end
+    -- make him invulnerable
+    memory.writebyte(RAM_hurtInvCount, 127)
+    -- manipulate Mario's position
+    memory.writeword(RAM_xPos, math.floor(x))
+    memory.writeword(RAM_yPos, math.floor(y))
+    memory.writeword(RAM_xSubPos, math.floor(x*16)%16*16)
+    memory.writeword(RAM_ySubPos, math.floor(y*16)%16*16)
+end
+
+local smwMoveMethodProc = { smwMoveNormalProc, smwMovePMeterProc, smwMoveFreeProc }
+
+-- force allow escape
+local smwForceSecretExit = false
+local smwModExitStatus = false
+function smwAllowEscape()
+    if smwPause and pad_down[smwPlayer].select then
+        local levelIndex = memory.readbyte(RAM_levelIndex)
+        local levelFlag = memory.readbyte(RAM_levelFlagTable + levelIndex)
+        -- save midway point flag
+        if memory.readbyte(RAM_midwayPoint) == 1 then
+            memory.writebyte(RAM_levelFlagTable + levelIndex, bit.bor(levelFlag, 0x40))
+        end
+        -- exit the level force
+        memory.writebyte(RAM_levelFlagTable + levelIndex, bit.bor(levelFlag, 0x80))
+        smwForceSecretExit = (pad_press[smwPlayer].A ~= nil)
+        -- activate next stage (destination must be written by smwRewriteOnPauseExit())
+        -- pressing B will cancel this effect (exit without activate the next level)
+        if not pad_press[smwPlayer].B then
+            memory.writebyte(RAM_activateNextLevel, 1)
+        else
+            memory.writebyte(RAM_activateNextLevel, 0)
+        end
+        smwModExitStatus = true
+    end
+end
+
+-- allow start+select to beat the level
+function smwRewriteOnPauseExit()
+    if not smwModExitStatus then return end
+    if memory.readbyte(RAM_typeOfExit) == 0x80 and memory.readbyte(RAM_activateNextLevel) == 1 then
+        if smwForceSecretExit then
+            memory.writebyte(RAM_typeOfExit, 2)
+        else
+            memory.writebyte(RAM_typeOfExit, 1)
+        end
+        smwModExitStatus = false
+    end
+end
+
+-- cut powerup animation
+function smwCutPowerupAnimation()
+    if smwMovement == 2 then -- super
+        memory.writebyte(RAM_lockSpritesTimer, 0)
+        memory.writebyte(RAM_movement, 0)
+        memory.writebyte(RAM_marioFrameCount, 0)
+        smwDoPowerUp() -- don't know why script needs to process it, anyway
+    elseif smwMovement == 3 then -- cape
+        memory.writebyte(RAM_lockSpritesTimer, 0)
+        memory.writebyte(RAM_movement, 0)
+        memory.writebyte(RAM_marioFrameCount, 0)
+    elseif smwMovement == 4 then -- fire
+        memory.writebyte(RAM_lockSpritesTimer, 0)
+        memory.writebyte(RAM_movement, 0)
+        memory.writebyte(RAM_marioFrameCount, 0)
+        memory.writebyte(0x7e149b, 0)
+    end
+end
+
+-- cut powerdown animation
+function smwCutPowerdownAnimation()
+    if smwMovement == 1 then -- powerdown
+        memory.writebyte(RAM_lockSpritesTimer, 0)
+        memory.writebyte(RAM_marioFrameCount, 0) -- stops 1 frame, but who cares
+        -- memory.writebyte(RAM_movement, 0)
+        -- memory.writebyte(RAM_hurtInvCount, 127)
+    end
+end
+
+-- [ game-specific main ] ------------------------------------------------------
+
+local preventItemPopup = false
+-- apply various cheats that work in Level
+function smwApplyLevelCheats()
+    -- power-ups
+    if not smwPause and pad_press[smwPlayer].up and pad_down[smwPlayer].select then
+        smwDoPowerUp()
+        preventItemPopup = true
+    end
+    -- moving method
+    if not smwPause and pad_press[smwPlayer].L and not pad_press[smwPlayer].R and pad_down[smwPlayer].A then
+        smwSetMoveMethod(smwMoveMethod + 1)
+    end
+    if not smwPause then
+        smwMoveMethodProc[smwMoveMethod+1]()
+    end
+
+    -- prevent item popup
+    if preventItemPopup and not pad_press[smwPlayer].select then
+        preventItemPopup = false
+    end
+    if preventItemPopup then
+        DeleteJoypadPresses(smwPlayer, "select")
+    end
+
+    -- cut powerup/powerdown animation
+    if smwCutPowerupAnimationOn then
+        smwCutPowerupAnimation()
+    end
+    if smwCutPowerdownAnimationOn then
+        smwCutPowerdownAnimation()
+    end
+
+    -- allow escape
+    smwAllowEscape()
+end
+
+-- apply various cheats
+function smwApplyCheats()
+    -- switch info display
+    local pressLenLR = math.min(pad_presstime[smwPlayer].L, pad_presstime[smwPlayer].R)
+    if smwShowInfoMoreKeySw and pressLenLR == 30 then
+        smwShowInfoMore = not smwShowInfoMore
+    end
+
+    if smwGameMode == gameMode_level then
+        smwApplyLevelCheats()
+    elseif smwGameMode == gameMode_ow then
+        smwMoveMethod = moveMethod_normal
+    elseif smwGameMode == 11 then
+        smwRewriteOnPauseExit()
+    end
+end
+
+-- draw P-meter in the screen
+local pMeterMaxCount = 0
+function smwDrawPMeter()
+    if not (smwGameMode == gameMode_level) then return end
+
+    local pMeter = memory.readbyte(RAM_pMeter)
+    local pMeterBarXPos = 8
+    local pMeterBarYPos = 208
+    local pMeterBarWidth = 112
+    local pMeterBarHeight = 6
+    local pMeterBarLen
+    local pMeterBorderColor = "#000000ff"
+    local pMeterMeterColorNormal = "#31bdc5cc"
+    local pMeterMeterColorMax = { "#ff0000cc", "#ff0000cc", "#ffffffcc" }
+    local pMeterMeterColor
+    local pMeterBGColor = "#00000080"
+
+    if pMeter >= pMeter_max or smwMoveMethod == moveMethod_pmeter then
+        pMeterMaxCount = (pMeterMaxCount + 1) % 3
+        pMeterMeterColor = pMeterMeterColorMax[1+pMeterMaxCount]
+        pMeterBarLen = pMeterBarWidth
+    else
+        pMeterColorPulse = 0
+        pMeterMeterColor = pMeterMeterColorNormal
+        pMeterBarLen = math.floor((pMeter/1.0)*pMeterBarWidth/pMeter_max)
+    end
+
+    gui.edgelessbox(pMeterBarXPos, pMeterBarYPos, pMeterBarXPos + pMeterBarWidth, pMeterBarYPos + pMeterBarHeight, "#000000")
+    gui.fillbox(pMeterBarXPos + 1, pMeterBarYPos + 1, pMeterBarXPos + pMeterBarWidth - 1, pMeterBarYPos + pMeterBarHeight - 1, pMeterBGColor)
+    if pMeter > 0 then
+        gui.fillbox(pMeterBarXPos + 1, pMeterBarYPos + 1, pMeterBarXPos + pMeterBarLen - 1, pMeterBarYPos + pMeterBarHeight - 1, pMeterMeterColor)
+    end
+end
+
+-- display some useful information
+function smwDisplayInfo()
+
+    smwDrawPMeter()
+
+    if not smwShowInfoMore then return end
+
+    if smwGameMode == gameMode_level then
+        -- show Mario's position
+        gui.text(120, 40, string.format("%4d.%03d,%3d.%03d",
+            memory.readword(RAM_xPos), memory.readbyte(RAM_xSubPos),
+            memory.readword(RAM_yPos), memory.readbyte(RAM_ySubPos)))
+    end
+end
+
+-- [ core ] --------------------------------------------------------------------
+
+emu.registerbefore(function()
+    scanJoypad()
+    smwScanStatus()
+    if not movie.active() then
+       if smwPlayer <= pad_max then
+           smwApplyCheats()
+       end
+       sendJoypad()
+    end
+end)
+
+emu.registerafter(function()
+end)
+
+emu.registerexit(function()
+    if not movie.active() and smwMoveMethod == moveMethod_free then
+        memory.writebyte(RAM_frozen, 0) -- unfreeze Mario
+    end
+end)
+
+gui.register(function()
+    smwDisplayInfo()
+end)
+
+while true do emu.frameadvance() end
