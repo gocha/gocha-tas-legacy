62149283f7fc4d67263f9d25cbbb579653962408
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2009-12-08 06:10:49 +0000

    sm2conv: error handling
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@99 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

--- Scripts/sm2conv.lua
+++ Scripts/sm2conv.lua
@@ -2,6 +2,7 @@
 
 require("smvlib")
 
+local mode, dstpath, srcpath
 if emu then
 	if true then print("Open this script with a text editor, comment out this line, and set proper path to the variables below") return end
 	mode = "-e" -- "-e" or "-d"
@@ -18,24 +19,42 @@ else
 	end
 end
 
+local smvfile, sm2file
+local smv, succeeded
 if mode == "-e" then
 	print("Now Loading...")
 	smvfile = io.open(dstpath, "rb")
 	smv = smvImport(smvfile)
 	smvfile:close()
+	if not smv then
+		io.stderr:write("Error: Failed to load a movie.\n")
+		return
+	end
 	print("Exporting SM2...")
 	sm2file = io.open(srcpath, "w")
-	sm2Export(smv, sm2file)
+	succeeded = sm2Export(smv, sm2file)
 	sm2file:close()
-	print("Done")
+	if succeeded then
+		print("Done")
+	else
+		io.stderr:write("Error: Failed to export a movie.\n")
+	end
 else
 	print("Now Loading...")
 	sm2file = io.open(dstpath, "r")
 	smv = sm2Import(sm2file)
 	sm2file:close()
+	if not smv then
+		io.stderr:write("Error: Failed to load a movie.\n")
+		return
+	end
 	print("Exporting SMV...")
 	smvfile = io.open(srcpath, "wb")
-	smvExport(smv, smvfile)
+	succeeded = smvExport(smv, smvfile)
 	smvfile:close()
-	print("Done")
+	if succeeded then
+		print("Done")
+	else
+		io.stderr:write("Error: Failed to export a movie.\n")
+	end
 end
