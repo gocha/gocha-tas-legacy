4f5b25593aa59de0428f89314cfda97b51239aa5
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2010-01-03 12:29:37 +0000

    HUD: removed junk lines
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@105 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

--- Scripts/hud_simple.lua
+++ Scripts/hud_simple.lua
@@ -13,19 +13,3 @@ gui.register(function()
 		lagframe
 	))
 end)
-
---[[
-movie.active ()
-Returns true if any movie file is open, or false otherwise.
-movie.recording ()
-Returns true if a movie file is currently recording, or false otherwise.
-movie.playing ()
-Returns true if a movie file is currently playing, or false otherwise.
-movie.mode ()
-Returns one of the following:
-
-    * "playback": a movie file is currently playing
-    * "record": a movie file is currently recording
-    * "finished": a movie file is done playing but still open
-    * nil: there is no movie file open 
-   ]]
