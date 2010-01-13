630fd2d62d0410ce26251776d4f76d438d6ee3b8
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2010-01-13 05:34:13 +0000

    add small utility function library for lua-gd
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@107 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

---
+++ Scripts/gdutil.lua
@@ -0,0 +1,40 @@
+-- small utility functions for lua-gd
+
+if not gd then
+	require "gd"
+end
+
+-- return if an image is a truecolor one
+gd.isTrueColor = function(im)
+	if im == nil then return nil end
+	local gdStr = im:gdStr()
+	if gdStr == nil then return nil end
+	return (gdStr:byte(2) == 254)
+end
+-- create a blank truecolor image
+gd.createTrueColorBlank = function(x, y)
+	local im = gd.createTrueColor(x, y)
+	if im == nil then return nil end
+
+	local trans = im:colorAllocateAlpha(255, 255, 255, 127)
+	im:alphaBlending(false)
+	im:filledRectangle(0, 0, im:sizeX() - 1, im:sizeY() - 1, trans)
+	im:alphaBlending(true) -- TODO: restore the blending mode to default
+	return im
+end
+-- return a converted image (source image won't be changed)
+gd.convertToTrueColor = function(imsrc)
+	if imsrc == nil then return nil end
+	if gd.isTrueColor(imsrc) then return imsrc end
+
+	local im = gd.createTrueColor(imsrc:sizeX(), imsrc:sizeY())
+	if im == nil then return nil end
+
+	im:alphaBlending(false)
+	local trans = im:colorAllocateAlpha(255, 255, 255, 127)
+	im:filledRectangle(0, 0, im:sizeX() - 1, im:sizeY() - 1, trans)
+	im:copy(imsrc, 0, 0, 0, 0, im:sizeX(), im:sizeY())
+	im:alphaBlending(true) -- TODO: set the mode which imsrc uses
+
+	return im
+end
