b415b0e24cc887441e0844f031e7339ffde6593d
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2010-10-23 12:51:25 +0000

    add a simple script, throw some trashes away
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@204 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

---
+++ Projects/AriaOfSorrow/LuaScripts/CVAoS_PositionDisplay.lua
@@ -0,0 +1,6 @@
+gui.opacity(0.68)
+gui.register(function()
+	local posX = memory.readwordsigned(0x0200a09a) * 0x10000 + memory.readdwordsigned(0x02000524)
+	local posY = memory.readwordsigned(0x0200a09e) * 0x10000 + memory.readdwordsigned(0x02000528)
+	gui.text(30, 18, string.format("%08X %08X", posX, posY))
+end)
