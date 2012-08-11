99e8592c3fd7c1571e81e49f659b196f1883d8df
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2012-08-11 00:44:21 +0000

    なんということでしょう
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@245 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

--- Scripts/emuluatest/emubasic.lua
+++ Scripts/emuluatest/emubasic.lua
@@ -1,35 +1,63 @@
-if emu.message then
-	emu.message("Hello")
-else
-	print("emu.message", "nil")
+function get()
+	if emu.message then
+		emu.message("Hello")
+	else
+		print("emu.message", "nil")
+	end
+
+	if emu.framecount then
+		print("emu.framecount", emu.framecount())
+	else
+		print("emu.framecount", "nil")
+	end
+
+	if emu.lagged then
+		print("emu.lagged", emu.lagged())
+	else
+		print("emu.lagged", "nil")
+	end
+
+	if emu.lagcount then
+		print("emu.lagcount", emu.lagcount())
+	else
+		print("emu.lagcount", "nil")
+	end
+
+	if emu.emulating then
+		print("emu.emulating", emu.emulating())
+	else
+		print("emu.emulating", "nil")
+	end
+
+	if emu.atframeboundary then
+		print("emu.atframeboundary", emu.atframeboundary())
+	else
+		print("emu.atframeboundary", "nil")
+	end
 end
 
-if emu.framecount then
-	print(emu.framecount())
-else
-	print("emu.framecount", "nil")
+get()
+
+firstbefore = true
+if emu.registerbefore then
+	emu.registerbefore(function()
+	if firstbefore then
+		firstbefore = false
+		print("------------------------------")
+		print("[emu.registerbefore]")
+		get()
+	end
+	end)
 end
 
-if emu.lagged then
-	print(emu.lagged())
-else
-	print("emu.lagged", "nil")
-end
-
-if emu.lagcount then
-	print(emu.lagcount())
-else
-	print("emu.lagcount", "nil")
-end
-
-if emu.emulating then
-	print(emu.emulating())
-else
-	print("emu.emulating", "nil")
-end
-
-if emu.atframeboundary then
-	print(emu.atframeboundary())
-else
-	print("emu.atframeboundary", "nil")
+if emu.registerafter then
+	firstafter = true
+	emu.registerafter(function()
+	if firstafter then
+		firstafter = false
+		print("------------------------------")
+		print("[emu.registerafter]")
+		get()
+	end
+	end)
 end
--- Scripts/emuluatest/guifunc.lua
+++ Scripts/emuluatest/guifunc.lua
@@ -2,19 +2,57 @@
 -- error("foo")
 
 if gui.parsecolor then
-	print(gui.parsecolor("#01020304"))
+	print("gui.parsecolor", gui.parsecolor("#01020304"))
 else
-	print("gui.parsecolor", "nil")
+	print("gui.parsecolor is not available")
 end
 
-print(gui.transparency)
+NYIAlert = {}
+function showAlert(name)
+	if not NYIAlert[name] then
+		print(name .. " is not available")
+		NYIAlert[name] = true
+	end
+end
+
+if not gui.transparency then
+	showAlert("gui.transparency")
+end
+
 gui.register(function()
-	gui.opacity(0.5)
-	gui.text(20, 20, "GUI test")
-	gui.opacity(1.0)
-	gui.pixel(40, 40, "red")
-	gui.line(40, 40, 80, 60, "#00ff0080")
-	gui.box(20, 100, 60, 120, "#ffff00", "#00ffff")
+	if gui.opacity then
+		gui.opacity(0.5)
+	else
+		showAlert("gui.opacity")
+	end
+
+	if gui.text then
+		gui.text(20, 20, "GUI test")
+	else
+		showAlert("gui.text")
+	end
+
+	if gui.opacity then
+		gui.opacity(1.0)
+	end
+
+	if gui.pixel then
+		gui.pixel(40, 40, "red")
+	else
+		showAlert("gui.pixel")
+	end
+
+	if gui.line then
+		gui.line(40, 40, 80, 60, "#00ff0080")
+	else
+		showAlert("gui.line")
+	end
+
+	if gui.box then
+		gui.box(20, 100, 60, 120, "#ffff00", "#00ffff")
+	else
+		showAlert("gui.box")
+	end
 end)
 
 -- TODO gui.getpixel
