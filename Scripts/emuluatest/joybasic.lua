b6f5d58f80d5e06292be99419c64395f50e7fd20
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2012-08-11 00:50:56 +0000

    ﾜﾝﾓｱｾｯ
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@246 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

--- Scripts/emuluatest/joybasic.lua
+++ Scripts/emuluatest/joybasic.lua
@@ -1,4 +1,4 @@
-local joyget = joypad.get
+local joyget = joypad.getdown
 -- get, getdown, getup
 -- peek, peekdown, peekup
 
@@ -8,6 +8,25 @@ emu.registerbefore(function()
 
 	-- redirect test
 	joypad.set(t)
+
+	local t2 = joyget()
+	-- must be t == t2, regardless of movie activity
+	local roundtripSuccess = true
+	for k in pairs(t) do
+		if t[k] ~= t2[k] then
+			roundtripSuccess = false
+			break
+		end
+	end
+	for k in pairs(t2) do
+		if t[k] ~= t[k] then
+			roundtripSuccess = false
+			break
+		end
+	end
+	if not roundtripSuccess then
+		emu.message("round-trip get/set error")
+	end
 end)
 
 -- 1. joypad.peek must return user input, regardless of movie activity
