977fb2ed94c7d37b302c849d50c55126f2fa95cd
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2012-12-28 06:48:25 +0000

    emuluatest: add strict test to gdalpha.lua
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@269 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

--- Scripts/emuluatest/gdalpha.lua
+++ Scripts/emuluatest/gdalpha.lua
@@ -1,4 +1,17 @@
 local gd = require("gd")
 local gdImage = gd.createFromGdStr(gui.gdscreenshot())
-gdImage:saveAlpha(true)
-gdImage:png("gdalpha.png") -- must be opaque!
+
+local totalAlpha = 0
+for y = 0, gdImage:sizeY() - 1 do
+	for x = 0, gdImage:sizeX() - 1 do
+		local color = gdImage:getPixel(x, y)
+		local alpha = gdImage:alpha(color)
+		if alpha ~= 0 then
+			print("alpha test failed! A(" .. x .. "," .. y .. ")=" .. alpha)
+			gdImage:saveAlpha(true)
+			gdImage:png("gdalpha.png") -- must be opaque!
+			return
+		end
+	end
+end
+print("alpha test passed!")
