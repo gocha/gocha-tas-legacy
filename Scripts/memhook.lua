e365817fa36725efd3a104297863078d7ea9be34
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2012-12-28 10:04:24 +0000

    Remove useless sample script
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@270 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

--- Scripts/memhook.lua
+++
@@ -1,7 +0,0 @@
-memory.registerread(0x7e00be, function(address, size)
-	print("read", string.format("%x", address), size)
-end)
-
-memory.registerwrite(0x7e00be, 2, function(address, size)
-	print("write", string.format("%x", address), size)
-end)
