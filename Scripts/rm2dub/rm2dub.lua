66034dcb7f9adfac2f730b8375775e21d96011a8
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2013-01-01 01:59:31 +0000

    rm2dub: add avisynth writer functions, convert samples to mono.
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@274 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

--- Scripts/rm2dub/rm2dub.lua
+++ Scripts/rm2dub/rm2dub.lua
@@ -1,42 +1,103 @@
-require("proAudioRt")
-if not proAudio.create() then os.exit(1) end
+-- Rockman 2: Maxim Kischine AudioDub Script
+-- Known Bugs: sfxs empty bug (I think something in FCEUX is wrong...)
 
-local sfxs =
+local printAVS = false
+
+local proAudioRt = require("proAudioRt")
+
+if not proAudio.create() then
+	error("proAudio initialization failed.")
+end
+
+-- audio file table
+local sfxFilenames =
 {
 	jump = {
-		proAudio.sampleFromFile("jump0.wav"),
-		proAudio.sampleFromFile("jump1.wav"),
-		proAudio.sampleFromFile("jump2.wav")
+		"jump0.wav",
+		"jump1.wav",
+		"jump2.wav"
 	},
 	damage = {
-		proAudio.sampleFromFile("damage1.wav"),
-		proAudio.sampleFromFile("damage2.wav")
+		"damage1.wav",
+		"damage2.wav"
 	},
 	die = {
-		proAudio.sampleFromFile("die1.wav"),
-		proAudio.sampleFromFile("die2.wav")
+		"die1.wav",
+		"die2.wav"
 	}
 }
 
+-- load audio files
+local sfxs = {}
+for groupName, filenames in pairs(sfxFilenames) do
+	sfxs[groupName] = {}
+	for i, filename in ipairs(filenames) do
+		sfxs[groupName][i] = proAudio.sampleFromFile(filename)
+	end
+end
+
+-- initialize RNG
 math.randomseed(os.time())
+
+-- AviSynth script dump, for video editing
+function writeAVS(line)
+	if printAVS then
+		print(line)
+	end
+end
+function writeAudioDubAVS(frame, audioName)
+	writeAVS(string.format("a = %s.DelayAudio(f2s(%d, last))", audioName, frame))
+	writeAVS(string.format("MixAudio(last, a, 1.0, 1.0)"))
+end
+-- and its first header part
+writeAVS("AviSource(\"filename.avi\")")
+writeAVS("function FrameToSeconds(int Frame, float FPS) {")
+writeAVS("  return Frame / FPS")
+writeAVS("}")
+writeAVS("function f2s(int Frame, clip c) {")
+writeAVS("  return FrameToSeconds(Frame, c.FrameRate)")
+writeAVS("}")
+for groupName, filenames in pairs(sfxFilenames) do
+	for i, filename in ipairs(filenames) do
+		writeAVS(string.format("%s%d_wav = WavSource(\"%s\")", groupName, i, filenames[i]))
+	end
+end
 
 local prevYVel = 0
 local prevHP = 0
+local frame = 0
+if emu.registerstart then
+	emu.registerstart(function()
+		-- prevent play sound on reset
+		prevYVel = 0
+		prevHP = 0
+	end)
+end
+
 emu.registerafter(function()
 	local yVel = memory.readbytesigned(0x0640)
 	local hp = memory.readbyte(0x06c0)
+
 	if yVel >= 4 and yVel ~= prevYVel then
-		proAudio.soundPlay(sfxs.jump[math.random(#sfxs.jump)])
+		local sfxIndex = math.random(#sfxs.jump)
+		proAudio.soundPlay(sfxs.jump[sfxIndex])
+		writeAudioDubAVS(frame, "jump" .. sfxIndex .. "_wav")
 	end
 	if hp < prevHP then
 		if hp == 0 then
-			proAudio.soundPlay(sfxs.die[math.random(#sfxs.die)])
+			local sfxIndex = math.random(#sfxs.die)
+			proAudio.soundPlay(sfxs.die[sfxIndex])
+			writeAudioDubAVS(frame, "die" .. sfxIndex .. "_wav")
 		else
-			proAudio.soundPlay(sfxs.damage[math.random(#sfxs.damage)])
+			local sfxIndex = math.random(#sfxs.damage)
+			proAudio.soundPlay(sfxs.damage[sfxIndex])
+			writeAudioDubAVS(frame, "damage" .. sfxIndex .. "_wav")
 		end
 	end
+
 	prevYVel = yVel
 	prevHP = hp
+	frame = frame + 1
 end)
 
 emu.registerexit(function()
