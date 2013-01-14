022418b5936828796eb3a8f26681aab4b91c4676
gochaism <gochaism@baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517>
2013-01-14 08:20:35 +0000

    OoE: add map image WIP
    git-svn-id: https://gocha-tas.googlecode.com/svn/trunk@278 baf8c1ce-7bc4-11de-bb5b-0fd25fbaf517

--- Projects/OrderOfEcclesia/LuaScripts/CVOoE_CombineMapImage.lua
+++ Projects/OrderOfEcclesia/LuaScripts/CVOoE_CombineMapImage.lua
@@ -1,5 +1,6 @@
 gd = require("gd")
 
+--[[
 local roomId = "M0200"
 local roomSegments = {
 	{ x = 1, y = 2 },
@@ -10,8 +11,47 @@ local roomSegments = {
 	{ x = 8, y = 1 },
 	{ x = 8, y = 2 }
 }
-local roomSize = { x = 256, y = 192 }
 local mapImageSize = { x = 9, y = 3 }
+local roomId = "M1200"
+local roomSegments = {
+	{ x = 1, y = 5 },
+	{ x = 10, y = 10 },
+	{ x = 10, y = 4 },
+	{ x = 10, y = 7 },
+	{ x = 12, y = 10 },
+	{ x = 12, y = 5 },
+	{ x = 14, y = 2 },
+	{ x = 15, y = 2 },
+	{ x = 3, y = 10 },
+	{ x = 3, y = 4 },
+	{ x = 4, y = 10 },
+	{ x = 4, y = 7 },
+	{ x = 5, y = 1 },
+	{ x = 5, y = 10 },
+	{ x = 5, y = 2 },
+	{ x = 5, y = 4 },
+	{ x = 6, y = 1 },
+	{ x = 6, y = 5 },
+	{ x = 6, y = 7 },
+	{ x = 7, y = 1 },
+	{ x = 7, y = 2 },
+	{ x = 8, y = 1 },
+	{ x = 8, y = 4 },
+	{ x = 9, y = 7 }
+}
+local mapImageSize = { x = 15, y = 10 }
+]]
+local roomId = "M0400"
+local roomSegments = {
+	{ x = 1, y = 1 },
+	{ x = 2, y = 1 },
+	{ x = 10, y = 1 },
+	{ x = 18, y = 1 },
+	{ x = 26, y = 1 }
+}
+local mapImageSize = { x = 26, y = 1 }
+
+local roomSize = { x = 256, y = 192 }
 
 -- create a blank truecolor image
 gd.createTrueColorBlank = function(x, y)
